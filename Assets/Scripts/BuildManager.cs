4f96a76b6ff7f819d8cd6962c56985d97992117a
Kanopuster <yusup.abdulyaparov@gmail.com>
2023-10-10 17:58:09 +0500

    +SpawnTurrets
    Спавнятся на node, используются скрипты buildmanager и node

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    void Awake ()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    //public GameObject buildEffect;
+    public GameObject turret;
+
+    //private TurretBlueprint turretToBuild;
+    private Node selectedNode;
+
+    //public NodeUI nodeUI;
+
+    public bool CanBuild { get { return turret != null; } }
+
+    public void SelectNode (Node node)
+    {
+        if (selectedNode == node)
+        {
+            DeselectNode();
+            return;
+        }
+
+        selectedNode = node;
+        turret = null;
+
+        //nodeUI.SetTarget(node);
+    }
+
+    public void DeselectNode()
+    {
+        selectedNode = null;
+       // nodeUI.Hide();
+    }
+
+    public void SelectTurretToBuild ()
+    {
+        
+        DeselectNode();
+    }
+
+    public GameObject GetTurretToBuild ()
+    {
+        return turret;
+    }
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    [HideInInspector]
+    public GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    BuildManager buildManager;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+
+        buildManager = BuildManager.instance;
+    }
+
+    public Vector3 GetBuildPosition()
+    {
+        return transform.position + positionOffset;
+    }
+
+    void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (turret != null)
+        {
+            buildManager.SelectNode(this);
+            return;
+        }
+
+        if (!buildManager.CanBuild)
+            return;
+
+        BuildTurret(buildManager.GetTurretToBuild());
+    }
+
+    void BuildTurret(GameObject turretPrefab)
+    {
+        GameObject _turret = Instantiate(turretPrefab, GetBuildPosition(), Quaternion.identity);
+        turret = _turret;
+
+        //GameObject effect = Instantiate(buildManager.buildEffect, GetBuildPosition(), Quaternion.identity);
+       // Destroy(effect, 5f);
+
+        Debug.Log("Turret build!");
+    }
+
+    void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (!buildManager.CanBuild)
+            return;
+
+        // Change the color or do other hover effects if needed.
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
